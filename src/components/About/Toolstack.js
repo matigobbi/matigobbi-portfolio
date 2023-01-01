536a56c262e75d61de6fa261b30806f0696ef17e
matigobbi <matigobbi@gmail.com>
2023-01-01 16:15:49 +0100

    style footer

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -11,19 +11,19 @@ import { FaGithub } from "react-icons/fa";
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiHeroku />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <FaGithub />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiNetlify />
       </Col>
     </Row>
