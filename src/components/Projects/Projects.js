e9ae13d45620ba193fc28fb6cf33628dbedc5492
matigobbi <matigobbi@gmail.com>
2023-01-08 16:12:43 +0100

    new projects added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+import arkanoid from "../../Assets/Projects/arkanoid.JPG"
+import liriks from "../../Assets/Projects/lyriks.JPG"
 import fahrrad from "../../Assets/Projects/fahrrad.jpg";
 import architrap from "../../Assets/Projects/architrap.JPG";
 import goalkeeper from "../../Assets/Projects/goalkeeper.JPG";
@@ -30,9 +32,19 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={liriks}
+              title="Lyriks || Spotify clone"
+              description="A complex music player developed using Redux, React, RapidAPI Shazam, Tailwind and Bootstrap components"
+              ghLink="https://github.com/matigobbi/Spotify-clone"
+              demoLink="https://lyriks-player.netlify.app/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               title="Architrap"
               imgPath={architrap}
-              description="website for architects immersed in the job market to leave reviews about their former employers and share comments."
+              description="Website for architects immersed in the job market to leave reviews about their former employers and share comments."
               ghLink="https://github.com/tkyngw/architrap"
               demoLink="http://architrap.herokuapp.com/"
             />
@@ -40,12 +52,22 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              isBlog={false}
               imgPath={goalkeeper}
-              title="The goalkeeper || Game"
+              title="The goalkeeper Game"
               description="Soccer penalty game, which has a history behind it. Javascript, canva, p5 are the resources used to build it."
               ghLink="https://github.com/matigobbi/my-Futgame"
               demoLink="https://matigobbi.github.io/my-Futgame/index1.html"              
+            />
+          </Col>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={arkanoid}
+              title="Arkanoid copy"
+              description="A game that i didn´t finish, i was working on it for fun and happy for it because i developed most of the logic of it. "
+              ghLink="https://github.com/matigobbi/Arkanoid-copy"
+              demoLink="https://arkanoid-copy.netlify.app/"              
             />
           </Col>
 
